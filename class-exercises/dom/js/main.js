5b08078bdcbde968346117e2866b0fe6059be255
fictionalspace <hannah.fu@sjsu.edu>
2024-03-12 14:05:12 -0700

    project 2 and other stuff I guess

---
+++ class-exercises/dom/js/main.js
@@ -0,0 +1,16 @@
+
+console.log("hello hello");
+
+let pageTitle = document.querySelector("#page-title");
+
+// JS timout changes h1 title after 3 seconds
+setTimeout(function(){
+    pageTitle.style.color = "pink";
+    //console.log("timeout worked!");
+}   , 3000)
+
+//click event on header changes background color
+document.querySelector("header").onclick = function() {
+    //console.log("clicked header");
+    document.querySelector("body").style.backgroundColor = "grey";
+}
